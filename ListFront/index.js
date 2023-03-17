f14b2a3cdf3506511be28a126a60547620839385
Dmitrii Sergeev <SerDimIgor@gmail.com>
2023-03-17 08:05:53 +0300

    add prefix -data for custom attribute

--- ListFront/index.js
+++ ListFront/index.js
@@ -1,8 +1,8 @@
 
-const lstTask = document.getElementById("list_task")
-const addTask = document.getElementById("add_task");
-const nameTask = document.getElementById("name_task");
-const dateMustDone = document.getElementById("date_must_done");
+const lstTask = document.getElementById("listTask")
+const addTask = document.getElementById("addTask");
+const nameTask = document.getElementById("nameTask");
+const dateMustDone = document.getElementById("dateMustDone");
 const URL_ADDRESS = "http://localhost:8080"
 
 async function makeRequest(url,method,dt = null) {
@@ -31,82 +31,81 @@ async function makeRequest(url,method,dt = null) {
 }
 
 async function deleteTask (e) {
-    console.log('deleteTask');
-    await makeRequest(`${URL_ADDRESS}/ListDo/DelTask`,'DELETE',e.target.getAttribute('del-button'));
+    await makeRequest(`${URL_ADDRESS}/ListDo/DelTask`,'DELETE',e.target.getAttribute('data-del-button'));
     renderTask();
 }
 
 const generateElement = (element) => {
     const flagStatus = element['isDone'] ? 'Готово' : 'В процессе))';
-    return `<div row-element="${element['id']}" class="row mt-4 d-flex justify-content-start">
-    <div task-name="${element['id']}" class="col-3">
+    return `<div data-row-element="${element['id']}" class="row mt-4 d-flex justify-content-start">
+    <div data-task-name="${element['id']}" class="col-3">
       ${element['task']}
     </div>
-    <div flag-status="${element['id']}" class="col-3">
+    <div data-flag-status="${element['id']}" class="col-3">
         ${flagStatus}
     </div>
-    <div must-done="${element['id']}" class="col-3">
+    <div data-must-done="${element['id']}" class="col-3">
         ${element['mustDone']}
     </div>
     <div class="col-3">
         <div class="row">
             <div class="col">
-                <button edit-button = "${element['id']}" class="btn btn-primary btnEdit"></button>
+                <button data-edit-button = "${element['id']}" class="btn btn-primary btnEdit"></button>
             </div>
             <div class="col">
-                <button del-button = "${element['id']}" class="btn btn-primary btnDel"></button>
+                <button data-del-button = "${element['id']}" class="btn btn-primary btnDel"></button>
             </div>
         </div>
     </div>
   </div>
-  <div flag-status-bool = "${element['id']}" style="display:none"> ${element['isDone']} </div>`
+  <div data-flag-status-bool = "${element['id']}" style="display:none"> ${element['isDone']} </div>`
 }
 
 
 async function updateData(id) {
-    const task_name = document.querySelector(`[edit-name-task = "${id}"]`).value;
-    const is_check = document.querySelector(`[edit-check-value = "${id}"]`).checked;
-    const time_done = document.querySelector(`[edit-date-must-done = "${id}"]`).value;
-    const dt = {'id' : id,'task' : task_name,'isDone' : is_check,'mustDone' : time_done};
+    const taskName = document.querySelector(`[data-edit-name-task = "${id}"]`).value;
+    const isCheck = document.querySelector(`[data-edit-check-value = "${id}"]`).checked;
+    const timeDone = document.querySelector(`[data-edit-date-must-done = "${id}"]`).value;
+    const dt = {'id' : id,'task' : taskName,'isDone' : isCheck,'mustDone' : timeDone};
     const res = await makeRequest(`${URL_ADDRESS}/ListDo/EditTask`,'PUT',dt);
 
     if (res!==null) {
-        const element = document.querySelector(`[edit-data-row = "${id}"]`);
+        const element = document.querySelector(`[data-edit-data-row = "${id}"]`);
         element.outerHTML = generateElement(res);
 
-        document.querySelector(`[del-button = "${id}"]`).addEventListener("click",deleteTask);
-        document.querySelector(`[edit-button = "${id}"]`).addEventListener("click",editTask)
+        document.querySelector(`[data-del-button = "${id}"]`).addEventListener("click",deleteTask);
+        document.querySelector(`[data-edit-button = "${id}"]`).addEventListener("click",editTask)
     }
 }
-const createEditForm = (id_record,task_name,flag_bool,time_done) =>{
-    const element = document.querySelector(`[row-element = "${id_record}"]`);
+const createEditForm = (idRecord,taskName,flagBool,timeDone) =>{
+    const element = document.querySelector(`[data-row-element = "${idRecord}"]`);
     console.log(element);
-    element.outerHTML = `<div edit-data-row="${id_record}" class="row mt-4 d-flex justify-content-start">
+    element.outerHTML = `<div data-edit-data-row="${idRecord}" class="row mt-4 d-flex justify-content-start">
         <div class="col-3">
-            <input edit-name-task="${id_record}" class = "form-control" placeholder = "Enter task" type = "text" value="${task_name}" >
+            <input data-edit-name-task="${idRecord}" class = "form-control" placeholder = "Enter task" type = "text" value="${taskName}" >
         </div>
         <div class="col-3">
-            <input class="form-check-input" type="checkbox" ${flag_bool === true ?'checked':''} edit-check-value = "${id_record}">
+            <input data-edit-check-value = "${idRecord}" class="form-check-input" type="checkbox" ${flagBool === true ?'checked':''}>
             <label class="form-check-label" for="flexCheckDefault">
                 Готово
             </label>
         </div>
         <div class="col-3">
-            <input edit-date-must-done = "${id_record}" class="form-control" type = "date" value="${time_done}" >
+            <input data-edit-date-must-done = "${idRecord}" class="form-control" type = "date" value="${timeDone}" >
         </div>
         <div class="col-3">
-            <button onClick="updateData(${id_record})" class="btn btn-primary btnSave"></button>
+            <button onClick="updateData(${idRecord})" class="btn btn-primary btnSave"></button>
         </div>
     </div>`    
 }
 
 function editTask(e) {
-    const id_data = e.target.getAttribute('edit-button'); 
-    const task_name = document.querySelector(`[task-name = "${id_data}"]`).innerText;
-    const time_done = document.querySelector(`[must-done = "${id_data}"]`).innerText;
-    const flag_text = document.querySelector(`[flag-status-bool = "${id_data}"]`).innerText;
-    const flag_bool = (flag_text.trim() === 'true');
-    createEditForm(id_data,task_name,flag_bool,time_done);
+    const idData = e.target.getAttribute('data-edit-button'); 
+    const taskName = document.querySelector(`[data-task-name = "${idData}"]`).innerText;
+    const timeDone = document.querySelector(`[data-must-done = "${idData}"]`).innerText;
+    const flagText = document.querySelector(`[data-flag-status-bool = "${idData}"]`).innerText;
+    const flagBool = (flagText.trim() === 'true');
+    createEditForm(idData,taskName,flagBool,timeDone);
 }
 
 async function renderTask() {
@@ -144,8 +143,8 @@ async function renderTask() {
         console.log(res);
         if(res!==null) {
             lstTask.innerHTML += generateElement(res);
-            document.querySelector(`[del-button = "${res['id']}"]`).addEventListener("click",deleteTask);
-            document.querySelector(`[edit-button = "${res['id']}"]`).addEventListener("click",editTask)
+            document.querySelector(`[data-del-button = "${res['id']}"]`).addEventListener("click",deleteTask);
+            document.querySelector(`[data-edit-button = "${res['id']}"]`).addEventListener("click",editTask)
     
         }
         nameTask.value = '';
